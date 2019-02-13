8a6770a93c41f67bc76c060b867f17c2680b6337
huesos <bonirosales@gmail.com>
2019-02-12 18:50:45 -0600

    creating function for onkeyup

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -37,6 +37,27 @@ function findSpaces(arrName){
 //
 function createBoard(){
     for (i = 0; i < arrGameName.length; i++){
-        
+        if (arrGameName[i] === " ") {
+            letterList = "<li style='border-bottom:none;margin:0 15px 0 5px' id='" + i + "' >" + arrGameName[i] + "</li>";
+            document.getElementById('game').innerHTML += letterList;
+        } else {
+            letterList = "<li id='" + i + "'></li>";
+            document.getElementById('game').innerHTML += letterList;
+        }
+    }
+}
+
+createBoard();
+
+document.onkeyup = function(event) {
+    // Turns event "key" to lowercase
+    var key = String.fromCharCode(event.keyCode < 91).toLowerCase(); 
+    // Check letters only
+    if (event.keyCode > 64 && event.keyCode < 91) {
+        if (game.guessed.indexOf(key) === -1) {
+            if (lowGameName.indexOf(key) > -1) {
+                
+            }
+        }
     }
 }
